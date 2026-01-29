9ab30c583c3c48837fd0af10182b86e44092c1b0
Jhon <jhonalvertmarbebe0@gmail.com>
2026-01-29 20:01:29 +0800

    done skill section

---
+++ js/content/skills.js
@@ -0,0 +1,67 @@
+export function skills() {
+  const SkillStore = {
+    SkillOne: `
+
+            <div class="skills active ">
+
+                            <div class="item">
+                                <i class="fa-brands fa-html5"></i>
+                                <span>HTML</span>
+                            </div>
+                            <div class="item">
+                                <i class="fa-brands fa-css"></i>
+                                <span>CSS</span>
+                            </div>
+                            <div class="item">
+                                <i class="fa-brands fa-js"></i>
+                                <span>Js</span>
+                            </div>
+                            <div class="item">
+                               <i class="fa-brands fa-bootstrap"></i>
+                                <span>Bootstrap</span>
+                            </div>
+                            <div class="item">
+                                <i class="fa-solid fa-database"></i>
+                                <span>Database</span>
+                            </div>
+                        </div>
+          
+          `,
+
+    SkillTwo: `
+          
+              
+
+          
+          
+          `,
+
+    SkillThree: `
+          
+           <div class="skills ">
+
+                            <div class="item">
+                               <i class="fa-brands fa-figma"></i>   
+                                <span>Figma</span>
+                            </div>
+                            <div class="item">
+                                <i class="fa-solid fa-pen-nib"></i> 
+                                <span>UI Design  </span>
+                            </div>
+                            <div class="item">
+                               <i class="fa-solid fa-layer-group"></i>   
+                                <span>Layout</span>
+                            </div>
+                            <div class="item">
+                               <i class="fa-solid fa-palette"></i> 
+                                <span>Color Theory  </span>
+                            </div>
+                            <div class="item">
+                              <i class="fa-solid fa-font"></i>   
+                                <span>Typography</span>
+                            </div>
+                        </div>
+
+          `,
+  };
+}
